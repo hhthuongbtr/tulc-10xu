f5142984b9f8176c694225036f12fc19b41b9ad8
thuonghh <thuonghh@vng.com.vn>
2021-05-13 14:47:24 +0700

    http handle

--- server/server-handle.go
+++ server/server-handle.go
@@ -1,1 +1,41 @@
 package server
+
+import (
+	"github.com/gin-gonic/gin"
+	issue_detect "github.com/hhthuongbtr/tulc-10xu/issue-detect"
+	"github.com/hhthuongbtr/tulc-10xu/telegram"
+	"log"
+)
+
+func (w *WebProxy) CallBack(ctx *gin.Context) {
+	//var subject string
+	//var body string
+	//var alarmMsg string
+	buf := make([]byte, 1024)
+	num, _ := ctx.Request.Body.Read(buf)
+	reqBody := string(buf[0:num])
+	log.Println(reqBody)
+	//var callbackData model.TencentAlarm
+	//ctx.BindJSON(&callbackData)
+	//switch callbackData.AlarmStatus {
+	//case "0":
+	//	switch callbackData.AlarmPolicyInfo.Conditions.EventName {
+	//	case "ping_unreachable":
+	//		subject = fmt.Sprintf("PROBLEM: [%s][HOST][%s]\n", callbackData.AlarmObjInfo.Dimensions.DeviceName, callbackData.AlarmPolicyInfo.Conditions.EventName)
+	//	}
+	//case "1":
+	//}
+
+	msgToSend := issue_detect.RefactorMessage(reqBody)
+	telegram.SendMsgToTelegram(&w.Conf, msgToSend)
+	ctx.String(200, msgToSend)
+	return
+}
+func (w *WebProxy) Ping(ctx *gin.Context) {
+	buf := make([]byte, 1024)
+	num, _ := ctx.Request.Body.Read(buf)
+	reqBody := string(buf[0:num])
+	log.Println(reqBody)
+	ctx.String(200, "pong")
+	return
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,65 @@
 package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"log"
+	"time"
+	"fmt"
+	"github.com/hhthuongbtr/tulc-10xu/configuration"
+)
+
+type WebProxy struct {
+	Conf	configuration.Conf
+}
+
+func RunAsHttpMode(conf configuration.Conf) {
+	log.Println("http mode, please wait")
+	webContext := WebProxy{
+		Conf: conf,
+	}
+	server := initializeServer()
+	setupRoute(server, &webContext)
+	log.Print("begin run http server...")
+	listenAdd := fmt.Sprintf("%s:%d", webContext.Conf.Server.Host, webContext.Conf.Server.Port)
+	log.Printf("serve on %s\n", listenAdd)
+	err := server.Run(listenAdd)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func setupRoute(server *gin.Engine, webContext *WebProxy) {
+	v1 := server.Group("/api/v1")
+	{
+		//----------------CCU-------------------
+		users := v1.Group("/Callback")
+		{
+			users.POST("", webContext.CallBack)
+			users.GET("", webContext.Ping)
+		}
+	}
+}
+
+func initializeServer() *gin.Engine {
+	server := gin.New()
+	gin.SetMode(gin.ReleaseMode)
+	server.Use(gin.Logger())
+	server.Use(gin.Recovery())
+
+	// CORS for https://foo.com and https://github.com origins, allowing:
+	// - PUT and PATCH methods
+	// - Origin header
+	// - Credentials share
+	// - Preflight requests cached for 30 seconds
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT"},
+		AllowHeaders:     []string{"Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           30 * time.Second,
+	}))
+	return server
+}
+
